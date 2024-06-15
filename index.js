d7872ec457f7a85911e06d2eb253b16e2587a91d
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-06-15 15:28:13 +0500

    done

---
+++ index.js
@@ -0,0 +1,87 @@
+// 1 - javob
+// alert(5 % 2);
+
+
+
+// // 2 - javob 
+// let randomson = Math.floor(Math.random()* 10);
+
+// alert(`randomson - ${randomson}`);
+
+
+
+
+// // 3 - javob 
+// let number = Math.floor(12.510);
+
+// alert(number);
+
+
+
+// 4-javob
+
+// function getlength(text) {
+//     return text.length;
+// }
+
+// getlength("Mars"); // 4
+
+// alert(getlength("Mars"));
+
+
+
+// 5-javob
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("Mars it school")
+// }
+
+// 6 - javob
+
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ];
+// ism = harflar[11] + harflar[0] + harflar[11] + harflar[19] + harflar[16] + harflar[8] + harflar[13] + harflar[12]
+
+// console.log(ism)
+
+// 7-javob
+
+// let yosh = Number(prompt("Yoshingizni kiriting"));
+
+// if (yosh > 18) {
+//     console.log(`Siz balog'atga yetgansiz!`)
+// } else {
+//     console.log(`Siz balog'atga yetmagansiz!`)
+// }  
+
+
+
+// 8 - javob 
+// let Ism = prompt("Ismingizni kiriting")
+
+// Ism = Ism.split("")
+
+// Ism = Ism.reverse()
+// Ism = Ism.join("")
+
+// alert(Ism)
+
+
+
+
+// // 10 - javob
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let toq = [];
+// let juft = [];
+
+// for (let i = 0; i < sonlar.length; i++) {
+//   const element = sonlar[i];
+//   if (element % 2 === 0) {
+//     juft.push(element);
+//   } else {
+//     toq.push(element);
+//   }
+// }
+
+// console.log(juft);
+// console.log(toq);
